b8d948a8288c13a5c283beb45d12cc89e6318195
AdrielMendelsohn <adrielmende@gmail.com>
2025-12-05 14:10:20 +0200

    OD-68 fixed a newline issue

--- Common/include/protocol/CommandResult.h
+++ Common/include/protocol/CommandResult.h
@@ -43,11 +43,7 @@ inline std::string HTTPAdapter(const CommandResult& result) {
     }
 
     const std::string& statusText = it->second;
-<<<<<<< HEAD
     return result.content.empty() ? statusText : statusText + "\n\n" + result.content + "\n";
-=======
-    return result.content.empty() ? statusText : statusText + "\n\n" + result.content;
->>>>>>> origin/main
 }
 
 #endif // COMMANDRESULT_H
